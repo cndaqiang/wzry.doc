9d010be026c404959cde1fc94a4dfabdb8e968d9
cndaqiang <who@cndaqiang.ac.cn>
2024-11-01 15:40:22 +0800

    test

--- docs/conf.py
+++ docs/conf.py
@@ -4,6 +4,6 @@ project = 'Your Project Name'  # 项目名称
 author = 'Your Name'  # 作者名称
 release = '0.1.0'  # 项目版本
 
-extensions = []  # 如果没有使用扩展，可以留空
+extensions = ["recommonmark"]  # 如果没有使用扩展，可以留空
 
 # 不需要指定模板路径和静态文件路径，使用默认设置即可
