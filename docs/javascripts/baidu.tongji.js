711618bdfd18974124a08ac0b2819657c553f81b
cndaqiang <who@cndaqiang.ac.cn>
2024-11-04 14:00:08 +0800

    auto Mon Nov  4 14:00:08     2024

---
+++ docs/javascripts/baidu.tongji.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?26b22c89693d6933677e3b62740ddf4b";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
