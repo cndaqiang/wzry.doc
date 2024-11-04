abb3070ec3b6a4f8708007ee532e77142460fe7b
cndaqiang <who@cndaqiang.ac.cn>
2024-11-04 16:35:38 +0800

    auto update: Mon Nov  4 16:35:38     2024

--- push.bat
+++ push.bat
@@ -1,2 +1,2 @@
 @echo off
-"C:\Program Files\Git\git-bash.exe" -c "git add .; git commit -m 'auto date: $(date)'; git push origin master; sleep 10" 
+"C:\Program Files\Git\git-bash.exe" -c "git add .; git commit -m \"auto update: $(date)\"; git push origin master; sleep 10" 
