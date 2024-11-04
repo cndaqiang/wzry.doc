7afcd531b4c5e8adab0d0e10e5705d2ed63126be
cndaqiang <who@cndaqiang.ac.cn>
2024-11-04 14:06:45 +0800

    Deployed 711618b with MkDocs version: 1.6.1

---
+++ javascripts/baidu.tongji.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?76d0fc6b8ffd3901ec51e8bece995b22";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
