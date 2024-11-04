6cc933ea938226ebb605ee203e7e2d5af755c2ba
cndaqiang <who@cndaqiang.ac.cn>
2024-11-04 16:32:40 +0800

    auto date: $(date)

--- push.bat
+++ push.bat
@@ -1,3 +1,3 @@
 @echo off
-"C:\Program Files\Git\git-bash.exe" -c "git add .; git commit -m 'auto date: $(date)'; git push" > git_log.txt 2>&1
+"C:\Program Files\Git\git-bash.exe" -c "git add .; git commit -m 'auto date: $(date)'; git push; sleep 100" > git_log.txt 2>&1
 notepad git_log.txt
