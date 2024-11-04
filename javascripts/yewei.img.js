fc4b75fed3de43ef0cdc2cc1eb987877b9d18160
cndaqiang <who@cndaqiang.ac.cn>
2024-11-04 15:41:00 +0800

    Deployed 24f1d46 with MkDocs version: 1.6.1

--- javascripts/yewei.img.js
+++ javascripts/yewei.img.js
@@ -1,6 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const domain = window.location.hostname; // 获取当前域名
-    const imgSrc = `https://count.getloli.com/@${domain}?name=${domain}&theme=green&padding=6&offset=0&align=top&scale=1&pixelated=1&darkmode=auto`;
+    // 在这里定义 fullUrl，确保它在 DOMContentLoaded 事件触发时可用
+    const fullUrl = window.location.href; // 获取当前完整 URL
+	
+    // 使用 URL 对象提取域名和路径
+    const url = new URL(fullUrl);
+    const domain = url.hostname; // 获取域名
+    const path = url.pathname.replace(/[^a-zA-Z0-9_]/g, ''); // 只保留字母、数字、下划线
+
+    // 组合域名和路径
+    const sanitizedUrl = `${domain}${path}`; // 组合成完整的安全 URL
+
+    // 检查构建的 URL
+    console.log("Sanitized URL: ", sanitizedUrl);
+	
+    const imgSrc = `https://count.getloli.com/@${sanitizedUrl}?theme=green&padding=6&offset=0&align=top&scale=1&pixelated=1&darkmode=auto`;
 
     // 创建一个容器元素，使图片居中对齐
     const imgContainer = document.createElement('div');
@@ -11,7 +24,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const img = document.createElement('img');
     img.src = imgSrc;
     img.alt = 'count';
-    img.style.maxWidth = '25%'; // 设置最大宽度为50%以调小图片
+    img.style.maxWidth = '25%'; // 设置最大宽度为25%以调小图片
     img.style.height = 'auto'; // 保持原始比例
     imgContainer.appendChild(img);
 
