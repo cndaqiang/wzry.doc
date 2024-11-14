6d088d20b47722da9a83154e2633943b3c97743c
cndaqiang <who@cndaqiang.ac.cn>
2024-11-14 19:00:42 +0800

    Automated commit by bot: Thu Nov 14 19:00:42     2024

--- push.bat
+++ push.bat
@@ -1,7 +1,9 @@
 "C:\Program Files\Git\git-bash.exe" -c "git add .; git commit -m \"Automated commit by bot: $(date)\"; git push origin master; sleep 10" 
 
+%USERPROFILE%\AppData\Local\anaconda3\python.exe indexNow.py
+
 ./push.ph.page.bat
 
-%USERPROFILE%\AppData\Local\anaconda3\python.exe indexNow.py
+
 echo Running script...
 pause
